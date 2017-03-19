0201ca804a76902e09c5bbb2e3a2e4fbb66f6b97
Sabrina Markon <phpsitescripts@outlook.com>
2017-03-19 07:36:42 +0000

    install live-server and remove sample /client/ c9 folder/app

--- dist/bundle.js
+++ dist/bundle.js
@@ -9443,6 +9443,10 @@ module.exports = ReactPropTypesSecret;
 "use strict";
 
 
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = __webpack_require__(52);
@@ -9477,13 +9481,15 @@ var App = function (_Component) {
             return _react2.default.createElement(
                 'div',
                 null,
-                'My App'
+                'APP'
             );
         }
     }]);
 
     return App;
 }(_react.Component);
+
+exports.default = App;
 
 /***/ }),
 /* 81 */
