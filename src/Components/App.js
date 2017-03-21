c10d40237d65de634e4daf3ac774ec787cd394ae
Sabrina Markon <phpsitescripts@outlook.com>
2017-03-21 05:56:51 +0000

    Get text from hipsterjesus API

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,39 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class App extends Component {
-    componentDidMount() {
-        console.log('React Running....');
+    // componentDidMount() {
+    //     console.log('React Running....');
+    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            paras: 4,
+            html: true,
+            text: ''
+        }
     }
+    
+    componentWillMount() {
+        this.getText();
+    }
+    
+    getText() {
+        axios.get('http://hipsterjesus.com/api?paras=' + this.state.paras + '&html=' + this.state.html)
+            .then((response) => {
+                this.setState({text: response.data.text}, function() {
+                    console.log(this.state);
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    
     render() {
         return(
             <div>
-                APP
+                My App
             </div>
             );
     }
